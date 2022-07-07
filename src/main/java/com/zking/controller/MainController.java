581df81969bd176f6852936647bbf9de04f76ec9
“18890176107” <18890176107@163.com>
2022-07-07 13:03:54 +0800

    springboot模板

---
+++ src/main/java/com/zking/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.zking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.security.PermitAll;
+
+@PermitAll
+@Controller
+public class MainController
+{
+    @GetMapping
+    public String index()
+    {
+        return "index";
+    }
+}
