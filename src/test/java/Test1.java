dc17697f074b96a0e4c1168f1a836e7e0fcbc87f
“18890176107” <18890176107@163.com>
2022-07-08 16:43:20 +0800

    springboot登录1.1

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,27 +1,36 @@
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.zking.App;
+import com.zking.entity.User;
 import com.zking.service.IActorService;
 import com.zking.service.ICommentService;
 import com.zking.service.IFilmService;
+import com.zking.service.impl.UserService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = App.class)
 public class Test1 {
     @Autowired
-    private  IActorService actorService;
+    private UserService userService;
     @Autowired
-    private  ICommentService commentService;
-    @Autowired
-    private  IFilmService filmService;
+    private PasswordEncoder encoder;
 
     @Test
     public void test1(){
-        System.out.println(filmService.getOne(null,false));
-        System.out.println(commentService.getOne(null,false));
-        System.out.println(actorService.getOne(null,false));
+        com.zking.entity.User user =
+                userService.getOne(new QueryWrapper<User>().eq("username", "admin"), false);
+        System.out.println(user);
+    }
+
+    @Test
+    public void test2(){
+        System.out.println(encoder.encode("admin"));
     }
 }
