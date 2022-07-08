0c4ee1899737e64af57d524f4c42352c4fa9dbb0
ChenTianLong <207752652@qq.com>
2022-07-08 10:44:05 +0800

    添加了一些查询语句

--- src/main/java/com/zking/service/impl/UserService.java
+++ src/main/java/com/zking/service/impl/UserService.java
@@ -1,11 +1,7 @@
 package com.zking.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.zking.entity.Authority;
-import com.zking.entity.Role;
 import com.zking.entity.User;
-import com.zking.repository.IAuthorityMapper;
-import com.zking.repository.IRoleMapper;
 import com.zking.repository.IUserMapper;
 import com.zking.service.IUserService;
 import lombok.RequiredArgsConstructor;
@@ -17,20 +13,16 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class UserService extends ServiceImpl<IUserMapper, User> implements IUserService {
-
-    private final IRoleMapper roleMapper;
-    private final IAuthorityMapper authorityMapper;
+    private final IUserMapper userMapper;
 
     //根据用户id查询，用户有哪些角色
-    @Override
-    public List<Role> getRolesByUserId(int id) {
-        return roleMapper.getRolesByUserId(id);
+    public List<String> findAllRoleByUserId(int id) {
+        return userMapper.findAllRoleByUserId(id);
     }
 
     //根据角色id查询，拥有哪些权限
-    @Override
-    public List<Authority> getAuthoritySByUserId(int id) {
-        return authorityMapper.getAuthoritySByUserId(id);
+    public List<String> findAllJurisdictionByUserId(int id) {
+        return userMapper.findAllJurisdictionByUserId(id);
     }
 
 
