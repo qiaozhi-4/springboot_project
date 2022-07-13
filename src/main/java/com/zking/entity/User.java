614683b8caab2c253784c222a127676d6c3f96e0
“18890176107” <18890176107@163.com>
2022-07-13 10:33:26 +0800

    主页以及yml上传文件路径

--- src/main/java/com/zking/entity/User.java
+++ src/main/java/com/zking/entity/User.java
@@ -1,14 +1,15 @@
 package com.zking.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.io.Serializable;
+import java.util.Collection;
 import java.util.Date;
 
 import static com.baomidou.mybatisplus.annotation.IdType.AUTO;
@@ -20,7 +21,7 @@ import static com.baomidou.mybatisplus.annotation.IdType.AUTO;
 //自动生成无参构造函数
 @NoArgsConstructor
 @TableName("user")
-public class User implements Serializable {
+public class User  implements UserDetails {
     @TableId(type = AUTO)
     private Integer id;
     private String username;
@@ -35,5 +36,35 @@ public class User implements Serializable {
     private String address;
     private String email;
     private String time;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return pass;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
 
