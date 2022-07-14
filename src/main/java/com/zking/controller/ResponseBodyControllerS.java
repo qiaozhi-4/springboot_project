c1af022a05715b3e46b946cf26bea086e1f44751
“18890176107” <18890176107@163.com>
2022-07-14 08:30:05 +0800

    更改数据库，实体类，mapper，service，以及主页获取电影数据

--- src/main/java/com/zking/controller/ResponseBodyControllerS.java
+++ src/main/java/com/zking/controller/ResponseBodyControllerS.java
@@ -2,14 +2,14 @@ package com.zking.controller;
 
 import com.zking.service.IFilmService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.PermitAll;
 import java.util.List;
 
 @PermitAll
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class ResponseBodyControllerS {
 
