21ac0cf3191517aea062d07ad12cf3fde4ed30b4
“18890176107” <18890176107@163.com>
2022-07-14 08:37:29 +0800

    更改数据库，实体类，mapper，service，以及主页获取电影数据

--- src/main/java/com/zking/entity/Comment.java
+++ src/main/java/com/zking/entity/Comment.java
@@ -23,6 +23,8 @@ public class Comment {
     private String conten;
     private Date time;
     private Double filmTime; // 发送弹幕时视频的播放时间
+    private String color; // 字体前景色
+    private String bgColor; // 字体背景色
 
 
 
