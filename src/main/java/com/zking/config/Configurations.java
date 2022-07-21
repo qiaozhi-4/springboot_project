2bbde7f24d07abc78533919326b23c8a517acad0
“18890176107” <18890176107@163.com>
2022-07-21 11:34:31 +0800

    更改数据库，实体类，mapper，service，以及主页获取电影数据

--- src/main/java/com/zking/config/Configurations.java
+++ src/main/java/com/zking/config/Configurations.java
@@ -29,7 +29,7 @@ public class Configurations {
     private String threadNamePrefix = "async_thread";
 
     // WebSocket唯一配置
-    //@Bean
+    @Bean
     public ServerEndpointExporter exporter() {
         return new ServerEndpointExporter();
     }
